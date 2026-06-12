9cd3399363b6b62f52170d049d4664556cdb8bfa
Alireza Pourgholam <alireza.pourgholam444@gmail.com>
2026-06-12 17:38:16 +0330

    fix: add module declarations for react-date-object subpath imports

---
+++ src/types/modules.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-date-object/calendars/persian';
+declare module 'react-date-object/calendars/gregorian';
+declare module 'react-date-object/locales/persian_fa';
+declare module 'react-date-object/locales/gregorian_en';
